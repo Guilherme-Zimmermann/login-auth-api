6daca44918ee4d62d491673af485138cf80cd689
GuiZimmermann <zimmerma83@gmail.com>
2024-04-26 09:53:11 -0400

    feat: user service

---
+++ src/main/java/com/example/loginauthapi/service/UserService.java
@@ -0,0 +1,20 @@
+package com.example.loginauthapi.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.loginauthapi.entities.User;
+import com.example.loginauthapi.repository.UserRepository; 
+
+@Service
+public class UserService {
+    
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+}
